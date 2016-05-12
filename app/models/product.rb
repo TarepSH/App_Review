57f4964562abdcb88d2a99936b8bc2d81253dbe7
TarepSH <tarepsh@gmail.com>
2016-05-12 22:47:06 +0000

    adding Style for the app

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
      has_many :reviews
 
     
-      has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+     	has_attached_file :image, styles: { medium: "800x500#" }
       validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
